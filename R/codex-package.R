c53115b74834d6f7c36a7c6f3e779a070793486a
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-01-26 13:44:44 +0100

    Update package description

---
+++ R/codex-package.R
@@ -0,0 +1,30 @@
+#' @details
+#' \tabular{ll}{
+#'  \strong{Package:} \tab codex \cr
+#'  \strong{Type:} \tab Package \cr
+#'  \strong{Version:} \tab 1.0.0 \cr
+#'  \strong{License:} \tab GPL-3 \cr
+#'  \strong{DOI:} \tab \href{https://doi.org/xxx}{xxx}
+#' }
+#' @author
+#' \strong{Full list of authors and contributors} (alphabetic order)
+#'
+#' \tabular{ll}{
+#'  Nicolas Frerebeau \tab \emph{Université Bordeaux Montaigne, France} \cr
+#' }
+#'
+#' \strong{Package maintainer}
+#'
+#' Nicolas Frerebeau\cr
+#' \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
+#'
+#' IRAMAT-CRP2A (UMR 5060)\cr
+#' Maison de l'Archéologie\cr
+#' Université Bordeaux Montaigne\cr
+#' F-33607 Pessac cedex\cr
+#' France
+#' @name dimensio-package
+#' @aliases dimensio
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
